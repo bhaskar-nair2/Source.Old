6c77c3a21718efdba225c6dece319c821cc0d3b9
bhaskar-nair2 <b.bhaskar.nair@gmail.com>
2018-07-14 18:01:04 +0530

    Index page front Ready

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, session
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
+app.secret_key= 'youKnowWhatJustFuckOff'
+app.config.from_pyfile('config.cfg')
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/search',methods=['POST'])
+def search():
+    return 'Hello World'
+
+
+@app.route('/downloads')
+def downloads():
+    return render_template('downloads.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    return 'Signed Up'
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    return 'Logged In'
 
 
 if __name__ == '__main__':
